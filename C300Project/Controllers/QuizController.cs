5cf0a2f30ba405ad82eb199664a52a9923b011d0
khairin syazwan <khairinsyazwan@outlook.com>
2021-11-20 15:18:48 +0800

    changes in account controller, login.cshtml,  account model. created quiz controller, quiz/attempt.cshtml and quiz/index.cshtml

---
+++ C300Project/Controllers/QuizController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using fyp.Models;
+using System.Dynamic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace fyp.Controllers
+{
+    public class QuizController : Controller
+    {
+        private AppDbContext _dbContext;
+
+        public QuizController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [Authorize]
+        public IActionResult Index()
+        {
+            DbSet<Quiz> dbs = _dbContext.Quiz;
+            List<Quiz> model = dbs.ToList();
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            if (!User.IsInRole("Admin"))
+                model = model.Where(so => so.CreatedBy == userId).ToList();
+            return View(model);
+        }
+    }
+}
