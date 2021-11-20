a4182668da10db7bf92c3a73ae5d8f74de51ffe6
ALFIE FARHAN B AHMAD SUHAIMI RP <19046587@myrp.edu.sg>
2021-11-20 12:19:11 +0800

    test efcore model creation

---
+++ C300 project/Models/Result.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace fyp.Models
+{
+    public partial class Result
+    {
+        public int ResultId { get; set; }
+        public int QuizId { get; set; }
+        public string AccountId { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Topic { get; set; }
+        public int Score { get; set; }
+        public bool Attempt { get; set; }
+        public DateTime Dt { get; set; }
+
+        public virtual Account Account { get; set; }
+    }
+}
