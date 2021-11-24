10389d1ad7bbaa36b532563c90cb745fd375aad6
ALFIE FARHAN B AHMAD SUHAIMI RP <19046587@myrp.edu.sg>
2021-11-24 23:25:59 +0800

    basic QuestionsController and update to index for viewing of questions

---
+++ C300Project/Controllers/QuestionsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using fyp.Models;
+using System.Dynamic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace fyp.Controllers
+{
+    public class QuestionsController : Controller
+    {
+        private AppDbContext _dbContext;
+
+        public QuestionsController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            DbSet<Question> dbs = _dbContext.Question;
+            List<Question> model = dbs.ToList();
+            return View(model);
+        }
+    }
+}
