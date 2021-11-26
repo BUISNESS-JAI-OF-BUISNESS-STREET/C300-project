05fc1e74fae8a2e09707651faaa1492c593b0958
khairin syazwan <khairinsyazwan@outlook.com>
2021-11-26 15:23:43 +0800

    added a create quiz function in the quiz controller added a role based security to assign create quiz for admin roles only
    change the role based for attempt quiz action to user
    
    added an update action method quiz in the quiz controller
    
    added a delete action method in the quiz controller
    
    added role based security set to admin
    
    codes in the action methods incomplete

--- C300Project/Controllers/QuizController.cs
+++ C300Project/Controllers/QuizController.cs
@@ -29,8 +29,8 @@ namespace fyp.Controllers
             return View(model);
         }
 
-        
-        [AllowAnonymous]
+
+        [Authorize(Roles = "User")]
         public IActionResult Attempt() //for users to attempt the quiz
         {
             //TODO: require attention
@@ -39,7 +39,7 @@ namespace fyp.Controllers
             return View(model);
         }
 
-        [AllowAnonymous]
+        [Authorize(Roles = "User")]
         [HttpPost]
         public IActionResult Attempt(Result result)
         {
@@ -71,22 +71,115 @@ namespace fyp.Controllers
             return View(model);
         }
 
-        /*
         [Authorize(Roles = "Admin")]
-        [HttpGet]
+        public IActionResult Create()
+        {
+            DbSet<Question> dbs = _dbContext.Question;
+            List<Question> model = dbs.ToList();
+            return View(model);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public IActionResult Create(Quiz quiz)
+        {
+            if (ModelState.IsValid)
+            {
+                var userId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+                _dbContext.Quiz.Add(quiz);
+                if (_dbContext.SaveChanges() == 1)
+                    TempData["Msg"] = "New quiz added!";
+                else
+                    TempData["Msg"] = "Failed to update database!";
+            }
+            else
+            {
+                TempData["Msg"] = "Invalid information entered";
+            }
+            return RedirectToAction("Index");
+        }
+
+        [Authorize]
         public IActionResult Update(int id)
         {
-            //TODO: require attention
+            DbSet<Quiz> dbs = _dbContext.Quiz;
+            Quiz quiz = dbs.Where(mo => mo.QuizId == id).FirstOrDefault();
+            
+
+            if (quiz != null)
+            {
+                DbSet<Pokedex> dbsPokes = _dbContext.Pokedex;
+                var lstPokes = dbsPokes.ToList();
+                ViewData["pokes"] = new SelectList(lstPokes, "Id", "Name");
+
+                return View(quiz);
+            }
+            else
+            {
+                TempData["Msg"] = "Shirt order not found!";
+                return RedirectToAction("Index");
+            }
         }
 
         [Authorize]
         [HttpPost]
         public IActionResult Update(ShirtOrder shirtOrder)
         {
-            //TODO: require attention
+            if (ModelState.IsValid)
+            {
+                DbSet<ShirtOrder> dbs = _dbContext.ShirtOrder;
+                ShirtOrder tOrder = dbs.Where(mo => mo.Id == shirtOrder.Id).FirstOrDefault();
+
+                if (tOrder != null)
+                {
+                    tOrder.Name = shirtOrder.Name;
+                    tOrder.Color = shirtOrder.Color;
+                    tOrder.PokedexId = shirtOrder.PokedexId;
+                    tOrder.Qty = shirtOrder.Qty;
+                    tOrder.Price = shirtOrder.Price;
+                    tOrder.FrontPosition = shirtOrder.FrontPosition;
+
+                    if (_dbContext.SaveChanges() == 1)
+                        TempData["Msg"] = "Shirt order updated!";
+                    else
+                        TempData["Msg"] = "Failed to update database!";
+                }
+                else
+                {
+                    TempData["Msg"] = "Shirt order not found!";
+                    return RedirectToAction("Index");
+                }
+            }
+            else
+            {
+                TempData["Msg"] = "Invalid information entered";
+            }
             return RedirectToAction("Index");
         }
-        */
+
+
+        [Authorize]
+        public IActionResult Delete(int id)
+        {
+            DbSet<ShirtOrder> dbs = _dbContext.ShirtOrder;
+
+            ShirtOrder tOrder = dbs.Where(mo => mo.Id == id)
+                                     .FirstOrDefault();
+
+            if (tOrder != null)
+            {
+                dbs.Remove(tOrder);
+                if (_dbContext.SaveChanges() == 1)
+                    TempData["Msg"] = "Shirt order deleted!";
+                else
+                    TempData["Msg"] = "Failed to update database!";
+            }
+            else
+            {
+                TempData["Msg"] = "Shirt order not found!";
+            }
+            return RedirectToAction("Index");
+        }
 
     }
 }
