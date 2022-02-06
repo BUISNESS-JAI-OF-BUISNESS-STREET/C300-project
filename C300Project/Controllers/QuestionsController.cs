8a2074ff9d3923260d3b62744d8aaca865ae20dc
ALFIE FARHAN B AHMAD SUHAIMI RP <19046587@myrp.edu.sg>
2022-02-07 03:40:29 +0800

    bug fixes

--- C300Project/Controllers/QuestionsController.cs
+++ C300Project/Controllers/QuestionsController.cs
@@ -194,12 +194,13 @@ namespace fyp.Controllers
 
                     QuizQuestionBindDb quizQuestionBind = new QuizQuestionBindDb();
                     var y = x + 1;
+                    var quizcheck = Convert.ToInt32(form["currAddId" + y]);
                     var radiocheck = Convert.ToInt32(form["Add" + y]);
 
-                    if(commonlist.Contains(y) && radiocheck == -1)
+                    if(commonlist.Contains(quizcheck) && radiocheck == -1)
                     {
-                        dbs2.Remove(dbs2.Where(mo =>mo.QuestionId == question.QuestionId && mo.QuizId == radiocheck).FirstOrDefault());
-                    }else if (commonlist.Contains(y) && radiocheck > 0)
+                        dbs2.Remove(dbs2.Where(mo =>mo.QuestionId == question.QuestionId && mo.QuizId == quizcheck).FirstOrDefault());
+                    }else if (commonlist.Contains(quizcheck) && radiocheck > 0)
                     {
                         continue;
                     }
@@ -340,7 +341,7 @@ namespace fyp.Controllers
         #endregion
 
         #region GetQtnTableUpdate
-        public IActionResult GetQuizTableUpdate(int questionId, int quizId)
+        public IActionResult GetQtnTableUpdate(int topicId, int quizId)
         {
             DbSet<Quiz> dbs = _dbContext.Quiz;
             DbSet<Topic> dbs2 = _dbContext.Topic;
@@ -364,7 +365,7 @@ namespace fyp.Controllers
 
             List<Question> question;
 
-            if (questionId == -1)
+            if (topicId == -1)
             {
                 question = dbs3
                     .Include(q => q.TopicNavigation)
@@ -375,7 +376,7 @@ namespace fyp.Controllers
             else
             {
                 question = dbs3
-                    .Where(q => q.Topic == questionId)
+                    .Where(q => q.Topic == topicId)
                     .Include(q => q.TopicNavigation)
                     .Include(q => q.SegmentNavigation)
                     .Include(q => q.UserCodeNavigation)
